fc2860684f50ab4b7d50e286f088c3699f5ab09f
“Kyle <kylemboomer@gmail.com>
2024-04-17 14:25:50 -0500

    First Functions and Gameboard Refactor

---
+++ main.js
@@ -0,0 +1,23 @@
+//querySelectors
+
+//Variables & Data Model
+var gameBoard = [0,1,2,3,4,5,6,7,8]
+var fireWins = [] 
+var waterWins = [] 
+
+//eventListeners
+
+//eventHandlers
+
+//Functions 
+function createPlayer(id, token, wins) {
+    return {
+        id, 
+        token, 
+        wins
+    }
+}
+
+function declareTurn() {
+
+}
