a824d53726a1c92dad6a79455b9608f9d7ec12dd
“Kyle <kylemboomer@gmail.com>
2024-04-17 16:36:22 -0500

    Add Label and Section

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
 //querySelectors
-
+var cells = document.querySelectorAll('[id^="cell"]')
+var playerSelect = document.getElementById('player-select')
 //Variables & Data Model
-var gameBoard = [0,1,2,3,4,5,6,7,8]
-var fireWins = [] 
-var waterWins = [] 
+var gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+var fireWins = []
+var waterWins = []
 
 //eventListeners
+cells.forEach(cell => {
+    cell.addEventListener('click', () => {
+        if (!cell.textContent) {
+            cell.textContent = currentPlayer
+            currentPlayer = currentPlayer === '🔥' ? ' 🌊 ' : '🔥'
+        }
+    })
+})
 
+playerSelect.addEventListener('change', () => {
+    var selectedPlayer = playerSelect.value
+    if (selectedPlayer === 'Fire') {
+        currentPlayer = createPlayer('currentPlayer', '🔥', 0)
+    } else if (selectedPlayer === 'Water') {
+        currentPlayer = createPlayer('currentPlayer', ' 🌊 ', 0) 
+    }
+})
 //eventHandlers
 
 //Functions 
 function createPlayer(id, token, wins) {
     return {
-        id, 
-        token, 
+        id,
+        token,
         wins
     }
 }
