5967510391900ce437cf315df4fc4334e5f7f540
“Kyle <kylemboomer@gmail.com>
2024-04-18 11:22:06 -0500

    Add Default Value to Select Element

--- main.js
+++ main.js
@@ -2,15 +2,16 @@
 var cells = document.querySelectorAll('[id^="cell"]')
 var playerSelect = document.getElementById('player-select')
 //Variables & Data Model
-var gameBoard = ['cell-0', 'cell-1', 'cell-2', 'cell-3', 'cell-4', 'cell-5', 'cell-6', 'cell-7', 'cell-8',  ]
+var gameBoard = []
 var fireWins = []
 var waterWins = []
+var currentPlayer =''
 
 //eventListeners
 cells.forEach(cell => {
     cell.addEventListener('click', () => {
         if (!cell.textContent) {
-            cell.textContent = currentPlayer
+            cell.textContent = currentPlayer.token
             currentPlayer = currentPlayer === '🔥' ? ' 🌊 ' : '🔥'
         }
     })
@@ -23,6 +24,8 @@ playerSelect.addEventListener('change', () => {
     } else if (selectedPlayer === 'Water') {
         currentPlayer = createPlayer('masterOfWater', ' 🌊 ', 0) 
     }
+    hide(player-select)
+    hide(h1)
 })
 //eventHandlers
 
